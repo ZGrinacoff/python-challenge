ca1d85ac5ac6398e98ec6cfd9e1c4b10c2a4418a
ZGrinacoff <zgrinacoff@gmail.com>
2019-06-07 13:01:28 -0700

    Removed function  and created lists for columns.

--- PyBank/main_PyBank.py
+++ PyBank/main_PyBank.py
@@ -4,12 +4,21 @@ import csv
 # Path to collect data from PyBank folder.
 pybank_csv = os.path.join('budget_data.csv')
 
+# Create empty lists to hold column(s) data.
+dates = []
+revenue = []
+
 # Read in the csv file.
-with open(pybank_csv,'r+') as csvfile:
+with open(pybank_csv,'r') as csvfile:
 
     # Split the data on columns.
     csvreader = csv.reader(csvfile,delimiter = ',')
 
     # Read the header row first.
     header = next(csvreader)
-    print(f"Header: {header}")
+    #print(f"Header: {header}")
+
+
+
+
+

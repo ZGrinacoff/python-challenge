8a17c3a16db0dd30fdd6058a089909f0cb7accde
ZGrinacoff <zgrinacoff@gmail.com>
2019-06-07 13:17:56 -0700

    Added count for months and variables for rev

--- PyBank/main_PyBank.py
+++ PyBank/main_PyBank.py
@@ -8,7 +8,7 @@ pybank_csv = os.path.join('budget_data.csv')
 dates = []
 revenue = []
 
-# Read in the csv file.
+# Read in the csv file and append data to lists.
 with open(pybank_csv,'r') as csvfile:
 
     # Split the data on columns.
@@ -17,6 +17,25 @@ with open(pybank_csv,'r') as csvfile:
     # Read the header row first.
     header = next(csvreader)
     #print(f"Header: {header}")
+
+    for row in csvreader:
+        dates.append(row[0])
+        revenue.append(int(row[1]))
+        #print(dates)
+
+# Count total months.
+total_months = len(dates)
+#print(total_months)
+# Correctly counted number of distinct months.
+
+# Create variables to hold greatest increase/decrease.
+# Set them equal to the first row/entry in list.
+# Then add each revenue to total revenue.
+greatest_increase = revenue[0]
+greatest_decrease = revenue[0]
+total_revenue = 0
+
+
 
 
 

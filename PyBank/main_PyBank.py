47b06da89ca4640ee8935eb1bb66d7cf18d5e940
ZGrinacoff <zgrinacoff@gmail.com>
2019-06-07 11:59:03 -0700

    Read in the csv file

--- PyBank/main_PyBank.py
+++ PyBank/main_PyBank.py
@@ -5,3 +5,11 @@ import csv
 pybank_csv = os.path.join('budget_data.csv')
 
 # Read in the csv file.
+with open(pybank_csv,'r+') as csvfile:
+
+    # Split the data on columns.
+    csvreader = csv.reader(csvfile,delimiter = ',')
+
+    # Read the header row first.
+    header = next(csvreader)
+    print(f"Header: {header}")

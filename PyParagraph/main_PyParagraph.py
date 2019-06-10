635070d351fc36c0e51e0f63370ab69f0f59cf56
ZGrinacoff <zgrinacoff@gmail.com>
2019-06-10 12:42:39 -0700

    + sent count, + asciiletters, removed punctuation

--- PyParagraph/main_PyParagraph.py
+++ PyParagraph/main_PyParagraph.py
@@ -16,4 +16,17 @@ with open (paragraph_txt, 'r') as txtfile:
 # Sentence count can be determined by locating punctuation: . or ! or ?
 # Add up all such cases to be equal to total sentence count.
 sent_count = paragraph.count('.') + paragraph.count('!') + paragraph.count('?')
-print(sent_count)
+#print(sent_count)
+
+# Create a string of letters(upper & lower) to reference when looping through txt.
+# This will help to remove characters that aren't actually letters.
+# Perform this task before trying to get word count.
+# Will later split paragraph into list which will make it more difficult to get total letter count later.
+letters = string.ascii_letters + " "
+#print(letters)
+
+for l in paragraph:
+    if l not in letters:
+        paragraph = paragraph.replace(l, '')
+#print(paragraph)
+

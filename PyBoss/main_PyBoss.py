583f2b8d6c8202cecef8cde9c551eba942535223
ZGrinacoff <zgrinacoff@gmail.com>
2019-06-10 10:06:17 -0700

    Format SSN, added state abb. dict at top of code.

--- PyBoss/main_PyBoss.py
+++ PyBoss/main_PyBoss.py
@@ -7,8 +7,62 @@ from datetime import datetime
 pyboss_csv = os.path.join('employee_data.csv')
 #print(pyboss_csv)
 
+# State abbreviation dictionary.
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
+
 # Empty lists to hold row data per column.
-Emp_ID = []
+emp_id = []
 names = []
 First_Name = []
 Last_Name = []
@@ -24,7 +78,7 @@ with open(pyboss_csv, 'r') as csvfile:
     for row in csvreader:
         
         # Drops all employee ids into new list.
-        Emp_ID = row[0]
+        emp_id.append(row[2])
 
         # Splits column Names into two separate First/Last names lists.
         names = row[1].split(' ')
@@ -35,14 +89,16 @@ with open(pyboss_csv, 'r') as csvfile:
 
         # Drops all dates into old date format list to be reformatted with datetime.
         old_date_format = row[2]
-        #print(old_date_format)
+        #print(type(old_date_format))
         datetimeobject = datetime.strptime(old_date_format,'%Y-%m-%d')
         new_date_format = datetimeobject.strftime('%m/%d/%Y')
-        #print(new_date_format)
+        #print(type(new_date_format))
 
         # Reformat SSN.
         ssn_old = row[3]
-        #print(type(ssn_old))
-        ssn_new = ssn_old.
+        ssn_new.append('***-**-' + ssn_old.split('-')[2])
+        #print(ssn_new)
+
+        
 
 

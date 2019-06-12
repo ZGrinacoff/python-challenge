a44f7c0bfe42f483f20dcfd1c4242ca01a911fd8
ZGrinacoff <zgrinacoff@gmail.com>
2019-06-12 09:30:05 -0700

    Final Commit

--- PyParagraph/main_PyParagraph.py
+++ PyParagraph/main_PyParagraph.py
@@ -42,6 +42,29 @@ for word in cleaned_paragraph:
 
 # Get total word count and set equL to new variable word count.
 word_count = len(cleaned_paragraph)
-print(word_count)
+#print(word_count)
 
+# Calculate average word length. 
+# Letter count/word count.
+letter_count_per_word = letter_count/word_count
+#print(letter_count_per_word)
 
+# Calculate words per sentence.
+# word count/sentence count
+avg_sentence_length = word_count/sent_count
+#print(avg_sentence_length)
+
+# Create output file path.
+output_file = os.path.join('paragraph_analysis_1.txt')
+
+# opens output file and writes to it
+with open(output_file, 'w') as txtfile:
+
+    txtfile.writelines('Paragraph Analysis\n-----------------\nApproximate Word Count: ' 
+                        + str(word_count)+ '\nApproximate Sentence Count: '+ str(sent_count) + 
+                        '\nAverage Letter Count: ' + str(letter_count_per_word) + 
+                        '\nAverage Sentence Length: ' + str(avg_sentence_length))
+
+# opens output file and prints to terminal
+with open(output_file, 'r') as txtout:
+    print(txtout.read())
